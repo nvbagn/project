58d93ca702201e3904739258437e3a14feb19204
lyh <1932428366@qq.com>
2023-02-28 20:11:55 +0800

    multithreaded

--- echoserver_py/server.py
+++ echoserver_py/server.py
@@ -2,21 +2,31 @@ import socket
 import threading
 
 def func():
-    client_fd, client_addr = server_fd.accept()
-
     while True:
-        info = client_fd.recv(10)
-        if not info:
-            break
-        print(info.decode("utf-8"))
-    
-    client_fd.close()
+        client_fd, client_addr = server_fd.accept()
+
+        while True:
+            print("connection from client %s" % str(client_addr))
+            info = client_fd.recv(10)
+            if not info:
+                break
+            print(info.decode("utf-8"))
+            
+        client_fd.close()
+        print("connection from client %s is closed" % str(client_addr))
 
 server_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_fd.bind(("0.0.0.0", 8888))
 server_fd.listen()
 
-for _ in range(10):
-    threading.Thread(target = func).start()
+threads = []
+for _ in range(3):
+    t = threading.Thread(target = func)
+    t.start()
+    threads.append(t)
+
+# Wait for all threads to finish
+for t in threads:
+    t.join()
 
 server_fd.close()
